e355ec710e90b79e5777900c4007b95eb507d047
Vladislav Medvedev <medvedev.v.a.27@gmail.com>
2017-03-05 11:00:38 +0000

    some improvments

--- santa/blib/lib/SecretSanta.pm
+++ santa/blib/lib/SecretSanta.pm
@@ -14,21 +14,34 @@ sub calculate {
 		if (ref $_) {
 			$members{$_->[0]}{spouse} = $_->[1];
 			$members{$_->[1]}{spouse} = $_->[0];
+			$members{$_->[0]}{gifter} = '';
+			$members{$_->[1]}{gifter} = '';
 		}
 		else {
-			$members{$_}{spouse} = 'no spouse';
+			$members{$_}{spouse} = '';
+			$members{$_}{gifter} = '';
 		}
 	}
 	
 	my $gifter = [keys %members]->[-1];
-	foreach my $key (keys %members) {
-		if (not $key eq $gifter and defined $members{$key}){
-			if ( not $gifter eq $members{$key}{spouse} and  not $key eq $members{$gifter}{spouse} ) {
-				push @res, [$gifter, $key];
-				if ($members{$key}{spouse} eq 'no spouse'){
-					$members{$gifter}{spouse} = $key;
+	for (1..2){
+		foreach my $key (keys %members) {
+			if (not $key eq $gifter){
+				if ( (not $gifter eq $members{$key}{spouse}) 
+				and  (not $key eq $members{$gifter}{spouse})
+				and (not $members{$gifter}{gifter})
+				) {
+					push @res, [$gifter, $key];
+					$members{$gifter}{gifter} = 1;
+					if ($members{$key}{spouse}){
+						$members{$gifter}{spouse} = $key;
+					}
+					else{
+						$members{$key}{spouse} = $gifter;
+					}
+					
+					$gifter = $key;
 				}
-				$gifter = $key;
 			}
 		}
 	}
