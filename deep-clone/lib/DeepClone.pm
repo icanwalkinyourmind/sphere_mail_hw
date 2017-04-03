46073e9a486d273d817908387c01afcdb6e3c4ce
Vladislav Medvedev <medvedev.v.a.27@gmail.com>
2017-04-03 22:47:36 +0300

    minor changes

--- deep-clone/lib/DeepClone.pm
+++ deep-clone/lib/DeepClone.pm
@@ -7,41 +7,41 @@ use DDP;
 
 
 sub save_scalar{
-	return my $new_scalar = shift;
+    return my $new_scalar = shift;
 }
 
 sub clone {
-	my $orig = shift;
-	my $refs = defined $_[0] ? $_[0] : {};
-	
-	if (ref $orig eq 'ARRAY') {
-		return $refs->{$orig} if exists $refs->{$orig};
-		my $new_array = [];
-		$refs->{$orig} = $new_array;
-		@{$new_array} = map clone($_, $refs), @{$orig};
-		return undef if defined $refs->{subref};
-		return $new_array;
-	}
-	elsif (ref $orig eq 'HASH') {
-		return $refs->{$orig} if exists $refs->{$orig};
-		my $new_hash = {};
-		$refs->{$orig} = $new_hash;
-		for my $k (sort {$a cmp $b} keys %{$orig}) {
-			$new_hash->{$k} = clone($orig->{$k}, $refs);
-		}
-		return undef if defined $refs->{subref};
-		return $new_hash;
-	}
-	elsif (not ref $orig) {
-		return save_scalar($orig);
-	}
-	elsif (ref $orig eq 'CODE') {
-		$refs->{subref} = '';
-		return undef;
-	} else {
-		return undef;
-	}
-	
+    my $orig = shift;
+    my $refs = defined $_[0] ? $_[0] : {};
+    
+    if (ref $orig eq 'ARRAY') {
+        return $refs->{$orig} if exists $refs->{$orig};
+        my $new_array = [];
+        $refs->{$orig} = $new_array;
+        @{$new_array} = map clone($_, $refs), @{$orig};
+        return undef if defined $refs->{subref};
+        return $new_array;
+    }
+    elsif (ref $orig eq 'HASH') {
+        return $refs->{$orig} if exists $refs->{$orig};
+        my $new_hash = {};
+        $refs->{$orig} = $new_hash;
+        for my $k (sort {$a cmp $b} keys %{$orig}) {
+            $new_hash->{$k} = clone($orig->{$k}, $refs);
+        }
+        return undef if defined $refs->{subref};
+        return $new_hash;
+    }
+    elsif (not ref $orig) {
+        return save_scalar($orig);
+    }
+    elsif (ref $orig eq 'CODE') {
+        $refs->{subref} = '';
+        return undef;
+    } else {
+        return undef;
+    }
+    
 }
 
 1;
